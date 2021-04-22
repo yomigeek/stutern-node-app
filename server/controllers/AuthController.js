3ef393f1dc53244c33cc0357b8035b0bd0bdc413
yomigeek <seayomi@gmail.com>
2021-04-22 14:21:35 +0100

    app

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,9 +1,35 @@
 import connect from "../database/conn";
+import {v4 as uuidv4} from "uuid";
+
 class AuthController {
   static userSignUp(req, res, next) {
+    const userId = uuidv4();
+    const {email, password, firstname, lastname, phone} = req.body;
     res.status(200).json({
       message: "success",
     });
+    connect.query(
+      `INSER INTO users (firstname, lastname, userid, email, password, role, phone)
+        VALUES (${firstname}, ${lastname}, ${userId}, ${email}, ${password}, 'user', ${phone})
+      `,
+      (err, response) => {
+        const result = JSON.parse(JSON.stringify(response.rows));
+
+        if (result) {
+          return res.status(201).json({
+            status: "success",
+            statusCode: 201,
+            message: "signup successful",
+          });
+        } else {
+          return res.status(400).json({
+            status: "error",
+            statusCode: 400,
+            message: "fail",
+          });
+        }
+      }
+    );
     // connect.query(
     //  `${'insert into users (firstname, lastname, userid, email, password, role, phone) ' +
     //     "values ('"}${'test'}', '${'test'}', '${req.body.name}','${req.body.name}','${'test'}','user', '${'test'}')`,
@@ -16,13 +42,13 @@ class AuthController {
     //       return res.status(201).json({
     //         status: "success",
     //         statusCode: 201,
-    //         message: "registered" 
+    //         message: "registered"
     //       });
     //     } else {
     //     return res.status(400).json({
     //         status: "error",
     //         statusCode: 400,
-    //         message: "fail" 
+    //         message: "fail"
     //       });
     //     }
     //   }
