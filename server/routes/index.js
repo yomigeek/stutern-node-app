665989366a41854c08321cd9c29c7fa5fe5fe785
yomigeek <seayomi@gmail.com>
2021-04-30 18:37:52 +0100

    add jwt

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import authRouter from './auth';
+import storyRouter from './story';
 
 const apiRoutes = Router();
 
 apiRoutes.use('/auth', authRouter);
+apiRoutes.use('/story', storyRouter);
 
 
 // Matches /api/v1 the API home route
